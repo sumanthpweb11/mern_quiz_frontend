77a19a8dceb83722c9c116eb1a3530f93584ed6a
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-02-14 17:41:11 +0530

    change

--- src/apiCalls/index.js
+++ src/apiCalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_PRO_MODE,
+  baseURL: process.env.REACT_APP_DEV_MODE,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
