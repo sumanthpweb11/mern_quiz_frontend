47ab8cdba9d8aa73712a8c66b89b21b89fb3517a
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-02-14 16:57:37 +0530

    changes

--- src/apiCalls/index.js
+++ src/apiCalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://mern-quiz-app-api.onrender.com",
+  baseURL: process.env.REACT_APP_PRO_MODE,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
